b75cf7618806ba0ed498e4dab63b9f34b6a993e4
ElvineJoy <elvinkuiyungest@gmail.com>
2024-06-04 11:42:44 +0300

    code along

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import ParentComponent from './ParentComponent';
+import Form from './Form';
 
 ReactDOM.render(
-  <Form />,
+  <ParentComponent />,
   document.getElementById('root')
 );
